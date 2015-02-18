ccd516b5220f6d6863f82efd934bf2692e8bbe60
Stefan Walther <swr@qlik.com>
2015-02-18 15:52:55 +0100

    Inital commit

---
+++ requirejs-css/src/qssample-requirejscssplugin.js
@@ -0,0 +1,64 @@
+/*global
+            define,
+            require,
+            window,
+            console,
+            _
+*/
+/*jslint    devel:true,
+            white: true
+ */
+define([
+        'jquery',
+        'underscore',
+        './properties',
+        './initialproperties',
+        './lib/js/extensionUtils',
+        'text!./lib/css/style.css'
+],
+function ($, _, props, initProps, extensionUtils, cssContent) {
+    'use strict';
+
+    extensionUtils.addStyleToHeader(cssContent);
+
+    return {
+
+        definition: props,
+        initialProperties: initProps,
+        snapshot: { canTakeSnapshot: true },
+        resize : function( $element, layout ) {
+            //do nothing
+        },
+//        clearSelectedValues : function($element) {
+//
+//        },
+
+
+        // Angular Support (uncomment to use)
+        //template: '',
+        // Angular Controller
+        //controller: ['$scope', function ($scope) {
+		//
+        //}],
+        // (Angular Controller)
+
+
+        paint: function ( $element, layout ) {
+
+            console.groupCollapsed('Basic Objects');
+            console.info('$element:');
+            console.log($element);
+            console.info('layout:');
+            console.log(layout);
+            console.groupEnd();
+
+            $element.empty();
+            var $helloWorld = $(document.createElement('div'));
+            $helloWorld.addClass('hello-world');
+            $helloWorld.html('Hello World from the extension "RequireJS CSS Plugin"');
+            $element.append($helloWorld);
+
+        }
+    };
+
+});
